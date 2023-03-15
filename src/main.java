09907d5ee2df302752d4f4330fa39933021e5866
ismar <ismar@MSI>
2023-03-15 17:05:38 -0600

    1

---
+++ src/main.java
@@ -0,0 +1,19 @@
+
+import Controladores.ListaController;
+import Modelos.ListaModel;
+import Vistas.frmListas;
+
+
+public class main {
+
+
+    public static void main(String[] args) {
+        frmListas VistaListas = new frmListas(null, true);
+        ListaModel ModeloLista = new ListaModel();
+        ListaController ControladorLista = new ListaController(VistaListas, ModeloLista);
+        
+        
+        
+    }
+    
+}
